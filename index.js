0279ae242db8cd26b1453f7ae5c3e76cd305457c
Tri0de <best108@uw.edu>
2021-03-16 20:11:07 -0700

    Added action code

---
+++ index.js
@@ -0,0 +1,84 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+// Run the task
+async function run() {
+  console.log('[ACTION] Running gradle build test.');
+
+  await runBuild(() => {
+    console.log('[ACTION] Running server test!');
+
+    let dir = './run/'
+
+    if (!fs.existsSync(dir))
+      fs.mkdirSync(dir);
+
+    fs.writeFile( './run/eula.txt', 'eula=true', { flag: 'wx' }, err => {
+      if(err)
+        core.setFailed(err.message);
+
+      runServer(() => {
+          console.log('[ACTION] Fabric server test has passed!')
+          runServer(() => {
+              console.log('[ACTION] Forge server test has passed!')
+              console.log('[ACTION] All tests have passed!')
+          }, 'forge');
+      }, 'fabric');
+    });
+  })
+}
+
+// Test building the mod
+async function runBuild(callback) {
+  if(!core.getInput('runBuildTest')) return;
+
+  let build;
+
+
+  if(process.platform === 'win32')
+    build = await spawn('cmd', ['/c', 'gradlew', 'build', '--refresh-dependencies'].concat(properties))
+  else
+    build = await spawn('./gradlew', ['build', '--refresh-dependencies'].concat(properties))
+
+
+  build.stdout.on('data', (data) => process.stdout.write(`${data}`));
+
+  build.stderr.on('data', (data) => process.stderr.write(`${data}`));
+
+  build.on('error', (err) => process.stderr.write(err))
+
+  // eslint-disable-next-line no-unused-vars
+  build.on('close', (code) => callback())
+}
+
+// Test running the server
+async function runServer(callback, serverType) {
+  if(!core.getInput('runServerTest')) return;
+
+  let server;
+
+  if(process.platform === 'win32')
+    server = await spawn('gradlew', [serverType.concat(':runServer'), '--args=“nogui”'].concat(properties), { shell: true});
+  else
+    server = await spawn('./gradlew', [serverType.concat(':runServer'), '--args=“nogui”'].concat(properties));
+
+  server.stdout.on('data', (data) => {
+    if (data.includes('For help, type')) {
+      console.log('[ACTION] Server test complete! Exiting process.')
+
+      if(process.platform === 'win32')
+        spawn("taskkill", ["/pid", server.pid, '/f', '/t']);
+      else
+        server.kill();
+    }
+
+    process.stdout.write(`${data}`)
+  });
+
+  server.stderr.on('data', (data) => process.stderr.write(`${data}`));
+
+  server.on('error', (err) => process.stderr.write(err))
+
+  // eslint-disable-next-line no-unused-vars
+  server.on('close', (code) => callback())
+}
